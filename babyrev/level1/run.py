c32bc5b048bcd499eb9568fd57f07280d0dd7c63
programmer-k <programmer-j@naver.com>
2021-09-05 20:39:22 +0900

    Add a solution for babyrev level1 challenge.

---
+++ babyrev/level1/run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+byte_list = '6a 73 6e'.split()
+
+for hexadecimal_value in byte_list:
+    decimal_val = int(hexadecimal_value, 16)
+    print(chr(decimal_val), end='')
+
+print()
